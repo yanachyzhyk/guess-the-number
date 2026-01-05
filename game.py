ba912266411a51f7db868380e30ab4f324816d0e
Yana Chyzhyk <yana.chyzhyk@gmail.com>
2026-01-05 14:25:09 +0300

    Fix: fix counter of attempts

--- game.py
+++ game.py
@@ -6,22 +6,26 @@ print("Добро пожаловать в числовую угадайку")
 def is_valid(s):
     return s in range(1, 101)
 
-
 def choose_number(num):
     number = random.randint(1, 100)
     i = 1
-    if num == number:
-        print("Ты угадал, поздравляю!")
-    while num != number:
+    while True:
+        
         if num > number:
             print("Слишком много")
         elif num < number:
             print("Слишком мало")
+        elif num == number:
+            print("Ты угадал, поздравляю!")
+            break
+            
         num = int(input("Вводи еще: "))
         i += 1
         if i > 5:
             print("Попытки закончились")
-            return
+            break
+    
+            
 
 
 num = int(input("Угадай моё число: "))
@@ -32,12 +36,16 @@ else:
     choose_number(num)
 
 
-again = "да"
-
-
-def again_game():
-    while again == "да":
-        number = random.randint(1, 100)
-        choose_number(number)
+again = input("Хочешь еще раз? (да/нет): ")
+while again == "да":
+    num = int(input("Угадай моё число: "))
+    if is_valid(num) is False:
+        print("А может быть все-таки введем целое число от 1 до 100?")
+        num = int(input("Угадай моё число: "))
+    else:
+        choose_number(num)
         again = input("Хочешь еще раз? (да/нет): ")
-    print("Пока!")
+print("Пока!")
+
+
+
